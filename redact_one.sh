03974e07942361021fecdd277f135df802f2965e
Ed Brannin <edbrannin@gmail.com>
2018-07-04 13:54:41 -0400

    Support filenames with spaces

--- redact_one.sh
+++ redact_one.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-FILENAME=$(basename $1)
+FILENAME=$(basename "$1")
 mkdir input output
-cp $1 input/
-python redact.py < input/$FILENAME > output/$FILENAME
-open output/$FILENAME
+cp "$1" input/
+python redact.py < "input/$FILENAME" > "output/$FILENAME"
+open "output/$FILENAME"
 cowsay Done
